35a4f168755e7eed36fa9718e9db1f6065017d81
N.Shimizu <chikoski@gmail.com>
2015-06-04 09:22:10 +0900

    問題文と実際のテストケースとに齟齬があったのを修正

--- src/js/05.js
+++ src/js/05.js
@@ -6,15 +6,15 @@
   describe("引数を全く取らない関数 n123 を次のように定義せよ", function(){
 
     it("配列を返す", function(){
-      expect(num123()).to.be.an("array");
+      expect(n123()).to.be.an("array");
     });
 
     it("返り値の長さは123", function(){
-      expect(num123().length).to.be(123);
+      expect(n123().length).to.be(123);
     });
 
     it("返り値の要素は全てtrueである", function(){
-      expect(num123().reduce(function(i, j){
+      expect(n123().reduce(function(i, j){
         return i && j;
       })).to.be(true);
     });
